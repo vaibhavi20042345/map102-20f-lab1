e03f6cbcc9db43734e02546220b3bd6a89d060fe
vaibhavi20042345 <20042345@saultcollege.ca>
2020-09-16 20:51:24 +0530

    UI label and button

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
